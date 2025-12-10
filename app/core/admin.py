15d59ffeea97b7f50827d05bd5d2a90e6eaa5a85
KyBarrioga <kybarrioga@gmail.com>
2025-12-10 14:45:43 -0700

    Feat: Integrated django admin customizations to api

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,34 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _ # noqa
+
+from core import models
 
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define admin model for custom User model"""
+
+    ordering = ['id']
+    list_display = ['username', 'name', 'email', 'is_staff']
+
+    # Customize the admin form layout
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal Info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login',)}),
+    )
+    readonly_fields = ('last_login',)
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'name',
+                       'is_staff', 'is_superuser', 'is_active'),
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
--- app/core/models.py
+++ app/core/models.py
@@ -23,9 +23,9 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, username, password):
+    def create_superuser(self, email, username, password, **extra_fields):
         """Create and return a new superuser."""
-        user = self.create_user(email, username, password)
+        user = self.create_user(email, username, password, **extra_fields)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
@@ -37,9 +37,12 @@ class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, default='')
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['email']
